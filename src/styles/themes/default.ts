51fe0351a26e01d9ae17e58d901a0c7b15903871
Rickson Simoes <rickson.simoes@hotmail.com>
2023-01-09 12:33:11 -0300

    :art: Global styles configuration and theme for styled components

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,20 @@
+export const defaultTheme = {
+  white: '#FFFFFF',
+  'gray-100': '#FAFAFA',
+  'gray-200': '#F3F2F2',
+  'gray-300': '#EDEDED',
+  'gray-400': '#E6E5E5',
+  'gray-500': '#D7D5D5',
+  'gray-600': '#8D8686',
+  'gray-700': '#574F4D',
+  'gray-800': '#403937',
+  'gray-900': '#272221',
+
+  yellowLight: '#F1E9C9',
+  yellow: '#DBAC2C',
+  yellowDark: '#C47F17',
+
+  purpleLight: '#EBE5F9',
+  purple: '#8047F8',
+  purpleDark: '#4B2995'
+}
