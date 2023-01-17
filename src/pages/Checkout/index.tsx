0dcebd78ffb55ba0cc6e0f79ada55faa6fdaec54
Rickson <rickson.simoes@hotmail.com>
2023-01-17 17:04:55 -0300

    :art: Checkout: Inputs - delivery address and payment

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,7 +1,81 @@
+import { CurrencyDollar, MapPinLine } from "phosphor-react";
+import { AddressInformations, Address, AdressDeliveryText, AdressInformationText, FormContainer, OrderContainer, Payment, AddressInputs, OptionalInput, InputRowCustomOne, InputRowCustomTwo, CustomInput } from "./styles";
+
 export function CheckoutPage() {
   return (
-    <div>
-      Checkout
-    </div>
+    <FormContainer>
+      <section>
+        <h4>Complete seu pedido</h4>
+
+        <OrderContainer>
+          <Address>
+            <AddressInformations>
+              <MapPinLine size={24} />
+
+              <AdressDeliveryText>
+                Endereço de Entrega
+              </AdressDeliveryText>
+
+              <span></span>
+
+              <AdressInformationText>
+                Informe o endereço onde deseja receber seu pedido
+              </AdressInformationText>
+            </AddressInformations>
+
+            <AddressInputs>
+              <div>
+                <CustomInput type="text" placeholder="CEP" />
+              </div>
+
+              <CustomInput type="text" placeholder="Rua" />
+
+              <InputRowCustomOne>
+                <CustomInput type="text" placeholder="Número" />
+
+                <OptionalInput>
+                  <CustomInput type="text" placeholder="Complemento" />
+                  <span>Opcional</span>
+                </OptionalInput>
+              </InputRowCustomOne>
+
+              <InputRowCustomTwo>
+                <CustomInput type="text" placeholder="Bairro" />
+                <CustomInput type="text" placeholder="Cidade" />
+                <CustomInput type="text" placeholder="UF" />
+              </InputRowCustomTwo>
+            </AddressInputs>
+          </Address>
+        </OrderContainer>
+
+        <OrderContainer>
+          <Payment>
+            <AddressInformations>
+              <CurrencyDollar size={24} />
+
+              <AdressDeliveryText>
+                Pagamento
+              </AdressDeliveryText>
+
+              <span></span>
+
+              <AdressInformationText>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </AdressInformationText>
+            </AddressInformations>
+          </Payment>
+        </OrderContainer>
+      </section>
+
+      <section>
+        <h4>Cafés selecionados</h4>
+
+        <div>
+          sdfasdfsdafads
+        </div>
+
+        <button type="submit">hum</button>
+      </section>
+    </FormContainer>
   )
 };
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,2 +1,111 @@
 import styled from "styled-components";
 
+export const FormContainer = styled.form`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  max-width: 70rem;
+  margin: 0 auto;
+  gap: 2rem;
+
+  > section {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+`
+
+export const OrderContainer = styled.div`
+  background-color: ${props => props.theme["gray-200"]};
+  border-radius: 6px;
+  padding: 2.5rem;
+  max-width: 40rem;
+`
+
+export const Address = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`
+
+export const AddressInformations = styled.div`
+    display: grid;
+    align-items: center;
+    column-gap: 0.5rem;
+    grid-template-columns: max-content 1fr;
+
+    svg {
+      color: ${props => props.theme.yellowDark}
+    }
+`
+
+export const AdressDeliveryText = styled.span`
+  color: ${props => props.theme["gray-800"]};
+`
+
+export const AdressInformationText = styled.span`
+  font-size: 0.875rem;
+`
+
+export const AddressInputs = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`
+
+export const CustomInput = styled.input`
+  padding: 0.725rem;
+  background-color: ${props => props.theme["gray-300"]};
+  border: 1px solid ${props => props.theme["gray-400"]};
+  border-radius: 6px;
+
+  &::placeholder {
+    font-size: 0.875rem;
+  }
+
+  &:focus {
+    outline: transparent;
+    box-shadow: 0 0 0 2px ${props => props.theme.yellow};
+  }
+`
+
+export const InputRowCustomOne = styled.div`
+  display: flex;
+  gap: 1rem;
+`
+
+export const OptionalInput = styled.div`
+  background-color: ${props => props.theme["gray-300"]};
+  border: 1px solid ${props => props.theme["gray-400"]};
+  display: grid;
+  grid-template-columns: 1fr min-content;
+  flex: 1;
+  align-items: center;
+
+  input {
+    border: none;
+  }
+
+  span {
+    font-size: 0.75rem;
+    font-style: italic;
+    margin-right: 0.5rem;
+    color: ${props => props.theme["gray-600"]}
+  }
+`
+
+export const InputRowCustomTwo = styled.div`
+  display: flex;
+  gap: 1rem;
+
+  input:nth-child(2) {
+    width: 17.25rem;
+  }
+
+  input:last-child {
+    width: 3.75rem;
+  }
+`
+
+export const Payment = styled.div`
+
+`
