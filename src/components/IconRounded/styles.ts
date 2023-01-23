dcf3417f214149970079ff352c228239bbaa312f
Rickson <rickson.simoes@hotmail.com>
2023-01-23 16:05:32 -0300

    :wrench: Renamed component

---
+++ src/components/IconRounded/styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const BGColor = {
+  purple: 'purple',
+  yellowDark: 'yellowDark',
+  yellow: 'yellow',
+  darkGray: 'gray-700',
+} as const;
+
+interface IItemIcon {
+  backgroundColor: keyof typeof BGColor;
+}
+
+export const ItemIcon = styled.div<IItemIcon>`
+  background: ${props => props.theme[BGColor[props.backgroundColor]]};
+  width: 2rem;
+  height: 2rem;
+  padding: 0.5rem;
+  border-radius: 50%;
+  color: ${props => props.theme.white};
+`
