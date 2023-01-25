3b74c531b59da83b3fdb3424abc2ab2a4cc740a5
Rickson <rickson.simoes@hotmail.com>
2023-01-25 12:16:02 -0300

    :hammer: Function TotalItemsInBasket enhanced to reduce the qty of items

--- src/contexts/basketContext.tsx
+++ src/contexts/basketContext.tsx
@@ -1,14 +1,7 @@
-import { createContext, Dispatch, useState } from "react";
-import { IProduct } from "../interfaces";
+import { createContext, useState } from "react";
+import { IBasketContextProvider, IBasketContextType, IItemsInBasket, IProduct } from "../interfaces";
+import { ConvertToCurrency } from "../Helpers/ConvertToCurrency"
 
-interface IBasketContextType {
-  products: IProduct[];
-  setAddProduct: (item: IProduct[]) => void;
-}
-
-interface IBasketContextProvider {
-  children: React.ReactNode
-}
 
 export const BasketContext = createContext({} as IBasketContextType);
 
@@ -19,9 +12,40 @@ export function BasketContextProvider({ children }: IBasketContextProvider) {
     setProducts(item);
   }
 
+  function TotalItemsInBasket(items: IProduct[]) {
+    const countedItems = items.reduce((acc, item) => {
+      const { name, price } = item;
+      const removeSpacesInName = name.replace(/\s/g, "");
+
+      const key = removeSpacesInName;
+      if (!acc[key]) {
+        acc[key] = {
+          name,
+          price: 0,
+          quantity: 0
+        };
+      }
+      acc[key].price += price;
+      acc[key].quantity = acc[key].quantity == 0 ? 1 : acc[key].price / item.price;
+
+      return acc;
+    }, {} as IItemsInBasket as any);
+
+    return Object.values(countedItems as IItemsInBasket[]);
+  }
+
+  const BasketItems = TotalItemsInBasket(products);
+  const TotalBasketItems = BasketItems.map(value => {
+    return {
+      ...value,
+      price: ConvertToCurrency(value.price),
+      quantity: Math.round(value.quantity)
+    }
+  })
+
   return (
     <BasketContext.Provider
-      value={{ products, setAddProduct }}>
+      value={{ products, setAddProduct, TotalBasketItems }}>
       {children}
     </BasketContext.Provider>
   )
