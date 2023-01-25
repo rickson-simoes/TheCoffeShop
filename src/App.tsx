7fdd1c5e02c9c173241df4d0c7f75884a0e556e5
Rickson <rickson.simoes@hotmail.com>
2023-01-25 09:40:34 -0300

    :wrench: BasketContext with Router as children

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { BasketContextProvider } from "./contexts/basketContext";
-
 import { Router } from "./Router";
 
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
+import { BasketContextProvider } from "./contexts/basketContext";
+
 export function App() {
   return (
-    <BasketContextProvider>
-      <ThemeProvider theme={defaultTheme} >
-        <GlobalStyle />
-        <BrowserRouter>
+    <ThemeProvider theme={defaultTheme} >
+      <BrowserRouter>
+        <BasketContextProvider>
           <Router />
-        </BrowserRouter>
-      </ThemeProvider>
-    </BasketContextProvider>
+        </BasketContextProvider>
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 };
