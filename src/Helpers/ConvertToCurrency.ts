236d6fb0a39894cb770798c97ba8025d8d673ec6
Rickson <rickson.simoes@hotmail.com>
2023-01-25 12:17:27 -0300

    :wrench: Enhancement of the function

--- src/Helpers/ConvertToCurrency.ts
+++ src/Helpers/ConvertToCurrency.ts
@@ -1,6 +1,5 @@
 export function ConvertToCurrency(price: number) {
-  const number = price;
-  const formattedNumber = number.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL', useGrouping: false });
+  const formattedNumber = price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL', useGrouping: false });
 
   const [, Price] = formattedNumber.split(" ");
 
