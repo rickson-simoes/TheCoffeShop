62ba87268daa0920477cb46fc481796988d443f6
Rickson <rickson.simoes@hotmail.com>
2023-01-25 16:22:15 -0300

    :hammer: Counter - selection of the coffees

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,12 +1,43 @@
 import { Minus, Plus } from "phosphor-react";
+import { useContext, useEffect, useState } from "react";
+import { BasketContext } from "../../contexts/basketContext";
+import { IProducts, IProduct } from "../../interfaces";
 import { CounterField } from "./styles";
 
-export function Counter() {
+export function Counter({ product }: IProducts) {
+  const [coffees, setCoffees] = useState<IProduct[]>([]);
+  const { products, setAddProduct } = useContext(BasketContext);
+  const totalCoffeesCounter = products.filter(value => value.name == product.name).length || 0;
+
+  useEffect(() => {
+    if (products.length > 0) {
+      const getAllCoffeesInsideProducts = products.filter(value => value.name == product.name);
+
+      setCoffees(getAllCoffeesInsideProducts)
+    }
+  }, [])
+
+  function handleCount(option: string) {
+    if (option == 'plus') {
+      setCoffees([...coffees, product]);
+      setAddProduct([...products, product])
+    }
+
+    if (option == 'minus' && totalCoffeesCounter > 0) {
+      const index = coffees.findIndex(value => value.name == product.name)
+      coffees.splice(index, 1);
+      setCoffees([...coffees]);
+
+      const updatedProducts = products.filter(value => value.name !== product.name);
+      setAddProduct([...updatedProducts, ...coffees]);
+    }
+  }
+
   return (
     <CounterField>
-      <button type='button'> <Minus weight="bold" /> </button>
-      <span> 1 </span>
-      <button type='button'> <Plus weight="bold" /> </button>
+      <button type='button' onClick={() => handleCount("minus")}> <Minus weight="bold" /> </button>
+      <span> {totalCoffeesCounter} </span>
+      <button type='button' onClick={() => handleCount("plus")}> <Plus weight="bold" /> </button>
     </CounterField>
   )
 };
