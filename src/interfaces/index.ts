9b89cea670e5ed08a72a4f910236f31f1fd965b9
Rickson <rickson.simoes@hotmail.com>
2023-01-25 12:16:55 -0300

    :hammer: Update with the interfaces

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -6,5 +6,23 @@ export interface IProduct {
 }
 
 export interface IProducts {
-  product: IProduct
+  product: IProduct;
+}
+
+export interface IBasketContextType {
+  products: IProduct[];
+  setAddProduct: (item: IProduct[]) => void;
+  TotalBasketItems: ITotalBasketItems[];
+}
+
+export interface IItemsInBasket {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export type ITotalBasketItems = Pick<IItemsInBasket, 'name' | 'quantity'> & { price: string };
+
+export interface IBasketContextProvider {
+  children: React.ReactNode
 }
