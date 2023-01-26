7cc4d680c11e31b9fcffd05c081046206f2b39c9
Rickson <rickson.simoes@hotmail.com>
2023-01-26 12:23:01 -0300

    :hammer: Counter implementation

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,12 +1,21 @@
 import { CurrencyDollar, MapPinLine, Trash } from "phosphor-react";
+import { useContext } from "react";
 
 import { Counter } from "../../components/Counter";
+import { BasketContext } from "../../contexts/basketContext";
+import { ConvertToCurrency } from "../../Helpers/ConvertToCurrency";
 import { Inputs } from "./Components/Inputs";
 import { PaymentOptions } from "./Components/PaymentOptions";
 import { SvgTitleInformation } from "./Components/SvgTitleInformation";
 import { ButtonCheckoutOrder, ButtonRemoval, FormContainer, ItemInformation, ItemMenu, List, OrderCheckout, OrderContainer, TotalFontSizeBigger, TotalOrder } from "./styles";
 
 export function CheckoutPage() {
+  const { TotalBasketItems } = useContext(BasketContext);
+
+  const totalItems = TotalBasketItems.reduce((acc, item) => acc + item.price, 0);
+  const totalDelivery = 3.50;
+  const totalValue = ConvertToCurrency(totalItems + totalDelivery);
+
   return (
     <FormContainer>
       <section>
@@ -40,34 +49,36 @@ export function CheckoutPage() {
 
         <OrderCheckout>
           <List>
-            <li>
-              <ItemMenu>
-                <img src="src\assets\CoffeTypeSvgs\Type=Expresso.svg" alt="" width={64} height={64} />
+            {TotalBasketItems.map((value) => (
+              <li key={value.id}>
+                <ItemMenu>
+                  <img src={`src\\assets\\CoffeTypeSvgs\\Type=${value.name}.svg`} alt="" width={64} height={64} />
 
-                <ItemInformation>
-                  <span>Expresso Tradicional</span>
-                  <div>
-                    <Counter />
-                    <ButtonRemoval type="button"><Trash /> <span>Remover</span></ButtonRemoval>
-                  </div>
-                </ItemInformation>
-              </ItemMenu>
+                  <ItemInformation>
+                    <span>{value.name}</span>
+                    <div>
+                      <Counter {...value} />
+                      <ButtonRemoval type="button"><Trash /> <span>Remover</span></ButtonRemoval>
+                    </div>
+                  </ItemInformation>
+                </ItemMenu>
 
-              <span>R$ 9,90</span>
-            </li>
+                <span>R$ {ConvertToCurrency(value.price)}</span>
+              </li>
+            )) ?? "No coffees selected"}
           </List>
 
           <TotalOrder>
             <div>
-              <span>Total de itens</span> <span>R$ 29,70</span>
+              <span>Total de itens</span> <span>R$ {ConvertToCurrency(totalItems)}</span>
             </div>
 
             <div>
-              <span>Entrega</span> <span>R$ 3,50</span>
+              <span>Entrega</span> <span>R$ {ConvertToCurrency(totalDelivery)}</span>
             </div>
 
             <div>
-              <TotalFontSizeBigger>Total</TotalFontSizeBigger> <TotalFontSizeBigger>R$ 33,20</TotalFontSizeBigger>
+              <TotalFontSizeBigger>Total</TotalFontSizeBigger> <TotalFontSizeBigger>R$ {totalValue}</TotalFontSizeBigger>
             </div>
           </TotalOrder>
 
