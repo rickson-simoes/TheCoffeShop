160c74f1fa118615885c415bb854475f7344014f
Rickson <rickson.simoes@hotmail.com>
2023-01-27 12:51:38 -0300

    :hammer: Refactor of counter, maintainable code

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,42 +1,28 @@
 import { Minus, Plus } from "phosphor-react";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { BasketContext } from "../../contexts/basketContext";
-import { IProduct } from "../../interfaces";
+import { ICoffee } from "../../interfaces";
 import { CounterField } from "./styles";
 
-export function Counter(product: IProduct) {
-  const [coffees, setCoffees] = useState<IProduct[]>([]);
-  const { products, setAddProduct } = useContext(BasketContext);
-  const totalCoffeesCounter = products.filter(value => value.id == product.id).length || 0;
+export function Counter({ ...coffeeItem }: ICoffee) {
+  const { addCoffee, removeCoffee, allCoffees } = useContext(BasketContext);
 
-  useEffect(() => {
-    if (products.length > 0) {
-      const getAllCoffeesInsideProducts = products.filter(value => value.id == product.id);
-
-      setCoffees(getAllCoffeesInsideProducts)
-    }
-  }, [])
+  const getCoffee = allCoffees.find(value => value.id == coffeeItem.id);
 
   function handleCount(option: string) {
     if (option == 'plus') {
-      setCoffees([...coffees, product]);
-      setAddProduct([...products, product])
+      addCoffee(coffeeItem);
     }
 
-    if (option == 'minus' && totalCoffeesCounter > 0) {
-      const index = coffees.findIndex(value => value.id == product.id)
-      coffees.splice(index, 1);
-      setCoffees([...coffees]);
-
-      const updatedProducts = products.filter(value => value.id !== product.id);
-      setAddProduct([...updatedProducts, ...coffees]);
+    if (option == 'minus') {
+      removeCoffee(coffeeItem);
     }
   }
 
   return (
     <CounterField>
       <button type='button' onClick={() => handleCount("minus")}> <Minus weight="bold" /> </button>
-      <span> {totalCoffeesCounter} </span>
+      <span> {getCoffee?.quantity || 0} </span>
       <button type='button' onClick={() => handleCount("plus")}> <Plus weight="bold" /> </button>
     </CounterField>
   )
